8180a7dc17f2cc8f0b94878b2c0a7dcf7ea72d78
Minami Ueda <minami.ueda@gmail.com>
2025-03-25 18:06:09 -0400

    Remove HTMLOutputBlock warning

--- src/components/HTMLOutputBlock/HTMLOutputBlock.js
+++ src/components/HTMLOutputBlock/HTMLOutputBlock.js
@@ -24,19 +24,6 @@ import Helmet from "react-helmet";
  */
 export const HTMLOutputBlock = ({ children, center = false }) => {
   console.log("center: ", center);
-  if (!children) {
-    console.warn("<HTMLOutputBlock/> should include a code block");
-    return null;
-  }
-
-  if (
-    !children.props?.children ||
-    children.props?.children?.props?.originalType !== "code"
-  ) {
-    console.warn("<HTMLOutputBlock/> should include a code block");
-    return null;
-  }
-
   const html = children.props.children.props.children;
 
   const script = html.match(/<script[^>]*>(.*)<\/script>/s);
