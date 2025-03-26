7366142b2e7dea0b746853289a9f75612f959d11
Minami Ueda <minami.ueda@gmail.com>
2025-03-26 01:26:24 -0400

    Don't change heading levels on render

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -53,11 +53,11 @@ const MDXComponents = {
   // その他のコンポーネント
   a: (props) => <Link {...props} />,
   h1: (props) => <Heading as="h1" {...props} />,
-  h2: (props) => <Heading as="h1" {...props} />,
-  h3: (props) => <Heading as="h2" {...props} />,
-  h4: (props) => <Heading as="h3" {...props} />,
-  h5: (props) => <Heading as="h4" {...props} />,
-  h6: (props) => <Heading as="h5" {...props} />,
+  h2: (props) => <Heading as="h2" {...props} />,
+  h3: (props) => <Heading as="h3" {...props} />,
+  h4: (props) => <Heading as="h4" {...props} />,
+  h5: (props) => <Heading as="h5" {...props} />,
+  h6: (props) => <Heading as="h6" {...props} />,
   Details: (props) => {
     let summaryContent = null;
     // 子要素を配列に変換
