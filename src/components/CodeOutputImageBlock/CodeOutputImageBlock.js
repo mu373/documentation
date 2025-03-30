1c1c392df188fcd144388a9257776927abf19471
Minami Ueda <minami.ueda@gmail.com>
2025-03-30 12:48:02 -0400

    Add svg image support

--- src/components/CodeOutputImageBlock/CodeOutputImageBlock.js
+++ src/components/CodeOutputImageBlock/CodeOutputImageBlock.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const CodeOutputImageBlock = ({ children, lang }) => {
+const CodeOutputImageBlock = ({ children }) => {
   
   const content = children;
 
   // Extract the image URL from the content
   const imageUrl = content.props.children.props.src
+  let imageClass = ""
+
+  if (imageUrl.endsWith('.svg')) {
+    imageClass += "svg "
+  }
   
   return (
     <div className="jupyter-output-image">
-      <img src={imageUrl} alt="output plot"/>
+      <img src={imageUrl} className={imageClass} alt="output plot"/>
     </div>
   );
 };
